11654648b2bbee7bc78130916ce4ce0de6c36e3b
marianoguilherme394-netizen <marianoguilherme394@gmail.com>
2026-06-25 17:08:07 -0300

    Comecei a fazer o condigo fonte do site

---
+++ JS/menu.js
@@ -0,0 +1,14 @@
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+function abrirMenu() {
+    sidebar.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function fecharMenu() {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+overlay.addEventListener("click", fecharMenu);
